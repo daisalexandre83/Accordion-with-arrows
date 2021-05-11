14a462f618228b3d14cc333f29e9563e0d064c93
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2021-05-11 11:37:30 -0300

    Criação e alteração de classes no CSS.Criação da função no Javascript.

--- js/main.js
+++ js/main.js
@@ -1,26 +1,18 @@
-$(function() {
-	var Accordion = function(el, multiple) {
-		this.el = el || {};
-		this.multiple = multiple || false;
+$(document).on('click','.menu',
+function(e){
+ e.stopPropagation();
 
-		// Variables privadas
-		var links = this.el.find('.link');
-		// Evento
-		links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
-	}
 
-	Accordion.prototype.dropdown = function(e) {
-		var $el = e.data.el;
-			$this = $(this),
-			$next = $this.next();
-
-		$next.slideToggle();
-		$this.parent().toggleClass('open');
-
-		if (!e.data.multiple) {
-			$el.find('.submenu').not($next).slideUp().parent().removeClass('open');
-		};
-	}	
-
-	var accordion = new Accordion($('#accordion'), false);
+$('.menu').not($(this)).removeClass('active');
+$(this).toggleClass('active');
+console.log('')
 });
+
+$(document).on('click',function() {
+$('.menu').removeClass('active');
+});
+
+$(document).on('click','.menu-content',
+function(e){
+	e.stopPropagation();
+})
