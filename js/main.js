32c6e95578f6e25c9efe396abc77e212de2e41a7
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2021-05-24 12:11:47 -0300

    Alterações no HTML e CSS, inclusão da pasta Fontawesome.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
-$(document).on('click','.menu',
-function(e){
- e.stopPropagation();
-
-
-$('.menu').not($(this)).removeClass('active');
-$(this).toggleClass('active');
-console.log('')
+$(document).on('click','.menu',function(e){
+ 	e.stopPropagation();
+	$('.menu').not($(this)).removeClass('active');
+	$(this).toggleClass('active');
+	console.log('oi')
 });
 
 $(document).on('click',function() {
-$('.menu').removeClass('active');
+	$('.menu').removeClass('active');
 });
 
 $(document).on('click','.menu-content',
-function(e){
-	e.stopPropagation();
+	function(e){
+		e.stopPropagation();
 })
